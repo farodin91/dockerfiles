93820467eb809fba5dc02288ebc91e85546102a9
Jan Jansen <farodin91@googlemail.com>
2014-12-29 22:40:18 +0100

    test

---
+++ minecraft/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -e /data/eula.txt ]; then
+	if [ "$EULA" != "" ]; then
+		echo "# Generated via Docker on $(date)" > eula.txt
+		echo "eula=$EULA" >> eula.txt
+	else
+		echo ""
+		echo "Please accept the Minecraft EULA at"
+		echo " https://account.mojang.com/documents/minecraft_eula"
+		echo "by adding the following immediately after 'docker run':"
+		echo " -e EULA=true"
+		echo ""
+		exit 1
+	fi
+fi
+
+
+java $JVM_OPTS -jar minecraft_server.jar
