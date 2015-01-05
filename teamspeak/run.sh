824589085e82a0a83464b3cb1e373c9154844293
Jan Jansen <farodin91@googlemail.com>
2015-01-05 22:01:12 +0100

    fixing

--- teamspeak/run.sh
+++ teamspeak/run.sh
@@ -8,27 +8,50 @@ if [ ! -e $DATA_DIR/state ]; then
 	mkdir $DATA_DIR/state/
 fi
 
-if [ ! -f $DATA_DIR/state/ts3server.sqlitedb ]; then
+echo "Database";
+
+if [ ! -e $DATA_DIR/state/ts3server.sqlitedb ]; then
 	touch $DATA_DIR/state/ts3server.sqlitedb
+fi
+if [ -e $DATA_DIR/state/ts3server.sqlitedb ]; then
 	ln -s $DATA_DIR/state/ts3server.sqlitedb $SERVER_DIR/ts3server.sqlitedb
 fi
 
-if [ ! -f $DATA_DIR/state/ts3server.ini  ]; then
-	$SERVER_DIR/ts3server_minimal_runscript.sh \
-	query_ip_whitelist="$DATA_DIR/state/query_ip_whitelist.txt" \
-	query_ip_backlist="$DATA_DIR/state/query_ip_blacklist.txt" \
-	logpath="$DATA_DIR/logs/" \
-	licensepath="$DATA_DIR/state/" \
-	inifile="$DATA_DIR/state/ts3server.ini" \
-	createinifile=1 
+echo "Blacklist";
+
+if [ -e $DATA_DIR/state/query_ip_blacklist.txt ]; then
+	ln -s $DATA_DIR/state/query_ip_blacklist.txt $SERVER_DIR/query_ip_blacklist.txt
+fi
+
+echo "Whitelist";
+
+if [ -e $DATA_DIR/state/query_ip_whitelist.txt ]; then
+	ln -s $DATA_DIR/state/query_ip_whitelist.txt $SERVER_DIR/query_ip_whitelist.txt
+fi 
+
+echo "files";
+
+if [ ! -e $DATA_DIR/files ]; then
+	mkdir $DATA_DIR/files/
+fi
+if [ -e $DATA_DIR/files ]; then
+	ln -sfv $DATA_DIR/files $SERVER_DIR/files
+fi
+
+echo "logs";
+
+if [ ! -e $DATA_DIR/logs ]; then
+	mkdir $DATA_DIR/logs/
+fi
+
+
+if [ -e $DATA_DIR/ts3server.ini ]; then
+	$SERVER_DIR/ts3server_minimal_runscript.sh start logpath=$DATA_DIR/logs/ inifile=$DATA_DIR/ts3server.ini
 else
-	$SERVER_DIR/ts3server_minimal_runscript.sh inifile="$DATA_DIR/state/ts3server.ini" 
+	$SERVER_DIR/ts3server_minimal_runscript.sh start logpath=$DATA_DIR/logs/ createinifile=1
 fi
 
 $SERVER_DIR/ts3server_linux_amd64
 
 
-if [ ! -e $DATA_DIR/logs ]; then
-	mkdir $DATA_DIR/logs/
-fi
 
