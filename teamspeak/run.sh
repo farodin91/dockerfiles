b1feffa3433bbdf8829f5da6086e71e8a86a2911
Jan Jansen <farodin91@googlemail.com>
2015-01-05 21:25:43 +0100

    test

--- teamspeak/run.sh
+++ teamspeak/run.sh
@@ -5,14 +5,29 @@ DATA_DIR=/data
 
 if [ ! -e $DATA_DIR/state ]; then
 	mkdir $DATA_DIR/state/
+fi
+
+if [ ! -f $DATA_DIR/state/ts3server.sqlitedb ]; then
 	touch $DATA_DIR/state/ts3server.sqlitedb
 	ln -s $DATA_DIR/state/ts3server.sqlitedb $SERVER_DIR/ts3server.sqlitedb
 fi
 
-if [ ! -e $DATA_DIR/state ]; then
-	mkdir $DATA_DIR/logs/
+if [ ! -f $DATA_DIR/state/ts3server.ini  ]; then
+	$SERVER_DIR/ts3server_minimal_runscript.sh \
+	query_ip_whitelist="/teamspeak3/query_ip_whitelist.txt" \
+	query_ip_backlist="/teamspeak3/query_ip_blacklist.txt" \
+	logpath="$DATA_DIR/logs/" \
+	licensepath="$DATA_DIR/state/" \
+	inifile="$DATA_DIR/state/ts3server.ini" \
+	createinifile=1 
 fi
 
+$SERVER_DIR/ts3server_linux_amd64
+
+
+if [ ! -e $DATA_DIR/logs ]; then
+	mkdir $DATA_DIR/logs/
+fi
 
 
 SERVER_DIR=/var/lib/teamspeak
