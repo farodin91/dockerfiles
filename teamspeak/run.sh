49022809f339918593a21fa273c27075fed3a9c2
Jan Jansen <farodin91@googlemail.com>
2015-01-05 20:42:56 +0100

    test

--- teamspeak/run.sh
+++ teamspeak/run.sh
@@ -1,4 +1,4 @@
 SERVER_DIR=/var/lib/teamspeak
 DATA_DIR=/data/config
 export LD_LIBRARY_PATH=$SERVER_DIR
-$SERVER_DIR/ts3server_linux_amd64 2>&1 | tee $DATA_DIR/logs/stderr.log
+$SERVER_DIR/ts3server_linux_amd64
