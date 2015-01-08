aa1f61e1737edc8f69260c770a7a2e6630d4a555
Jan Jansen <farodin91@googlemail.com>
2015-01-08 18:18:39 +0100

    php

--- php/run.sh
+++ php/run.sh
@@ -8,21 +8,22 @@ echo  $NGINX_DIR/$NAME_LINK_NGINX
 if [ ! -e $NGINX_DIR/$NAME_LINK_NGINX ]; then
 echo "Create NGINX file"
 	touch $NGINX_DIR/$NAME_LINK_NGINX
-	echo "upstream $NAME_LINK_NGINX{" >> $NGINX_DIR/$NAME_LINK_NGINX
-    echo "	 server $NAME_LINK_NGINX:$PORT;" >> $NGINX_DIR/$NAME_LINK_NGINX
-    echo "}\n" >> $NGINX_DIR/$NAME_LINK_NGINX
+#	echo "upstream $NAME_LINK_NGINX{" >> $NGINX_DIR/$NAME_LINK_NGINX
+#    echo "	 server $NAME_LINK_NGINX:$PORT;" >> $NGINX_DIR/$NAME_LINK_NGINX
+#    echo "}\n" >> $NGINX_DIR/$NAME_LINK_NGINX
 
     echo "server { ">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "   listen 80;">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "   location ~ \.php$ {">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "      fastcgi_index index.php;">> $NGINX_DIR/$NAME_LINK_NGINX
-    echo "      fastcgi_pass @$NAME_LINK_NGINX;">> $NGINX_DIR/$NAME_LINK_NGINX
+    echo "      fastcgi_pass @$NAME_LINK_NGINX:9000;">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "      include fastcgi_params;">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "   }">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "} ">> $NGINX_DIR/$NAME_LINK_NGINX
 fi
 
 sed 's/;daemonize = yes/daemonize = no/' -i /etc/php5/fpm/php-fpm.conf
+
 
 echo "RUN php-fpm"
 php5-fpm -F
