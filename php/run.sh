498238a87a33e6156e35945e13f9475eafc68a43
Jan Jansen <farodin91@googlemail.com>
2015-01-08 19:40:06 +0100

    php

--- php/run.sh
+++ php/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NGINX_DIR=/opt/nginx
+CONFIG_DIR=/opt/config
 PORT=9000
 
 echo  $NGINX_DIR/$NAME_LINK_NGINX 
@@ -15,8 +16,9 @@ echo "Create NGINX file"
     echo "server { ">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "   listen 80;">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "   location ~ \.php$ {">> $NGINX_DIR/$NAME_LINK_NGINX
+    echo "      fastcgi_split_path_info ^(.+\.php)(/.+)$;" >> $NGINX_DIR/$NAME_LINK_NGINX
     echo "      fastcgi_index index.php;">> $NGINX_DIR/$NAME_LINK_NGINX
-    echo "      fastcgi_pass @$NAME_LINK_NGINX:9000;">> $NGINX_DIR/$NAME_LINK_NGINX
+    echo "      fastcgi_pass $NAME_LINK_NGINX:9000;">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "      include fastcgi_params;">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "   }">> $NGINX_DIR/$NAME_LINK_NGINX
     echo "} ">> $NGINX_DIR/$NAME_LINK_NGINX
@@ -24,6 +26,23 @@ fi
 
 sed 's/;daemonize = yes/daemonize = no/' -i /etc/php5/fpm/php-fpm.conf
 
+if [ -e $CONFIG_DIR/php.ini ]; then
+    cp $CONFIG_DIR/php.ini /etc/php5/fpm/php.ini
+else
+    cp /etc/php5/fpm/php.ini $CONFIG_DIR/php.ini
+fi
+
+if [ -e $CONFIG_DIR/php-fpm.conf ]; then
+    cp $CONFIG_DIR/php-fpm.conf /etc/php5/fpm/php-fpm.conf
+else
+    cp /etc/php5/fpm/php-fpm.conf $CONFIG_DIR/php-fpm.conf
+fi
+
+if [ -e $CONFIG_DIR/www.conf ]; then
+    cp $CONFIG_DIR/www.conf /etc/php5/fpm/pool.d/www.conf
+else
+    cp /etc/php5/fpm/pool.d/www.conf $CONFIG_DIR/www.conf
+fi
 
 echo "RUN php-fpm"
 php5-fpm -F
