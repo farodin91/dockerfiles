3f0775c5312344f33e34501a9c8ea22efd77e32b
Jan Jansen <farodin91@googlemail.com>
2015-01-08 13:35:03 +0100

    ghost

--- ghost/run.sh
+++ ghost/run.sh
@@ -1,5 +1,22 @@
 
+NGINX_DIR=/opt/nginx
+GHOST_NAME_LINK_NGINX=${GHOST_NAME_LINK_NGINX:ghost}
+GHOST_PORT=2368
 
+if [ ! -e $NGINX_DIR/$GHOST_NAME_LINK_NGINX ]; then
+	touch $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+	echo "upstream $GHOST_NAME_LINK_NGINX{\n" >> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "	 $GHOST_NAME_LINK_NGINX:$GHOST_PORT;\n" >> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "}\n\n" >> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+
+    echo "server {\n ">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "   listen 80 default_server;">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "   listen [::]:80 default_server;">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "   location / {">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "      proxy_pass http://ghost;">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "   }">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "}\n ">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+fi
 
 
 bash /ghost-start
