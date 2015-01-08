a145254348dfa96ce9cdfba3bf53a6c68c671477
Jan Jansen <farodin91@googlemail.com>
2015-01-08 16:05:02 +0100

    ghost

--- ghost/run.sh
+++ ghost/run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 NGINX_DIR=/opt/nginx
 #GHOST_NAME_LINK_NGINX=${GHOST_NAME_LINK_NGINX:ghost}
@@ -5,17 +6,17 @@ GHOST_PORT=2368
 
 if [ ! -e $NGINX_DIR/$GHOST_NAME_LINK_NGINX ]; then
 	touch $NGINX_DIR/$GHOST_NAME_LINK_NGINX
-	echo "upstream $GHOST_NAME_LINK_NGINX{\n" >> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
-    echo "	 server $GHOST_NAME_LINK_NGINX:$GHOST_PORT;\n" >> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
-    echo "}\n\n" >> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+	echo "upstream $GHOST_NAME_LINK_NGINX{" >> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "	 server $GHOST_NAME_LINK_NGINX:$GHOST_PORT;" >> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "}\n" >> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
 
-    echo "server {\n ">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "server { ">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
     echo "   listen 80 default_server;">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
     echo "   listen [::]:80 default_server;">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
     echo "   location / {">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
     echo "      proxy_pass http://ghost;">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
     echo "   }">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
-    echo "}\n ">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
+    echo "} ">> $NGINX_DIR/$GHOST_NAME_LINK_NGINX
 fi
 
 
