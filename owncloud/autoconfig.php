8b1612e70b46a8794ab5f4f3eb190c896dcddca6
Jan Jansen <farodin91@googlemail.com>
2015-01-18 19:04:07 +0100

    owncloud

---
+++ owncloud/autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "pgsql",
+  "dbname"        => "{{POSTGRES_DB}}",
+  "dbuser"        => "{{POSTGRES_USER}}",
+  "dbpass"        => "{{POSTGRES_PW}}",
+  "dbhost"        => "{{POSTGRES_DB}}",
+  "dbtableprefix" => "",
+  "adminlogin"    => "{{ROOT_NAME}}",
+  "adminpass"     => "{{ROOT_PW}}",
+  "directory"     => "/var/www/data",
+);
