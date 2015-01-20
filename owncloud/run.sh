7d5ce82fbf8b889a40c0aae78795fd65f7069999
Jan Jansen <farodin91@googlemail.com>
2015-01-20 21:11:54 +0200

    playing with nginx

--- owncloud/run.sh
+++ owncloud/run.sh
@@ -5,37 +5,28 @@ echo "START OWNCLOUD"
 TMP_NGINX_DIR="/tmp/nginx"
 CONFIG_DIR="/opt/config"
 NGINX_CONF="/etc/nginx/nginx.conf"
-
+EDIT_NGINX_CONF="/opt/nginx/nginx.conf"
 
 echo "SETUP ENV"
 
 DATA_DIR=${DATA_DIR:-/var/www}
-NAME_LINK_NGINX=${NAME_LINK_NGINX:-owncloud}
-FASTCGI_PORT=${FASTCGI_PORT:-9000}
-PORT=${PORT:-80}
-CONFIGURE_NGINX=${CONFIGURE_NGINX:-true}
 USE_SSL=${USE_SSL:-false}
 NGINX_MAX_UPLOAD_SIZE=${NGINX_MAX_UPLOAD_SIZE:-20m}
-INDEX_PHP=${INDEX_PHP:-index.php}
 SERVER_NAME=${SERVER_NAME:-localhost}
 
-if [ ! -e $NGINX_CONF ]; then
-    if [ "$CONFIGURE_NGINX" = "true" ]; then
-        echo "ADD NGINX"
+echo "CONFIGURE NGINX"
 
-        if [ "$USE_SSL" = "true"]; then
-            cp $TMP_NGINX_DIR/owncloud-ssl $NGINX_CONF
-        else
-            cp $TMP_NGINX_DIR/owncloud $NGINX_CONF
-        fi
-        sed 's/{{SERVER_NAME}}/'"${SERVER_NAME}"'/' -i $NGINX_CONF
-        sed 's/{{NAME_LINK_NGINX}}/'"${NAME_LINK_NGINX}"'/' -i $NGINX_CONF
-        sed 's/{{FASTCGI_PORT}}/'"${FASTCGI_PORT}"'/' -i $NGINX_CONF
-        sed 's/{{PORT}}/'"${PORT}"'/' -i $NGINX_CONF
-        sed 's/{{INDEX_PHP}}/'"${INDEX_PHP}"'/' -i $NGINX_CONF
-        sed 's/{{NGINX_MAX_UPLOAD_SIZE}}/'"${NGINX_MAX_UPLOAD_SIZE}"'/' -i $NGINX_CONF
-        sed 's/{{ROOT}}/'"${DATA_DIR}"'/' -i $NGINX_CONF
+if [ ! -e $EDIT_NGINX_CONF ]; then
+    if [ "$USE_SSL" = "true"]; then
+        cp $TMP_NGINX_DIR/owncloud-ssl $NGINX_CONF
+    else
+        cp $TMP_NGINX_DIR/owncloud $NGINX_CONF
     fi
+    sed 's/{{SERVER_NAME}}/'"${SERVER_NAME}"'/' -i $NGINX_CONF
+    sed 's/{{NGINX_MAX_UPLOAD_SIZE}}/'"${NGINX_MAX_UPLOAD_SIZE}"'/' -i $NGINX_CONF
+    cp $EDIT_NGINX_CONF $NGINX_CONF
+else
+    cp $NGINX_CONF $EDIT_NGINX_CONF
 fi
 
 chown -R www-data:www-data /var/www/owncloud/*
