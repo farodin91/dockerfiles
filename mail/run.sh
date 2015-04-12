e6e2d4115bbde28568cc9a6d6521a9da342ab1b2
Jan Jansen <farodin91@googlemail.com>
2015-04-12 17:12:22 +0200

    update mail

---
+++ mail/run.sh
@@ -0,0 +1,8 @@
+
+DOVECOT_MAIN_CONF="/etc/dovecot/dovecot.conf"
+TMP_DOVECOT_MAIN_CONF="/tmp/dovecot/dovecot.conf"
+EDIT_DOVECOT_MAIN_CONF="/data/config/dovecot/dovecot.conf"
+TMP_SLAPD_CONF="/tmp/slapd.conf"
+EDIT_ODBC_CONF="/data/config/odbc.ini"
+ODBC_CONF="/etc/odbc.ini"
+TMP_ODBC_CONF="/tmp/odbc.ini"
