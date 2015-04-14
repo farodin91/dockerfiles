13073651fa45d0f92b87e1c68f974a478c1ed9af
Jan Jansen <farodin91@googlemail.com>
2015-04-14 10:52:14 +0200

    update doveoct

--- mail/run.sh
+++ mail/run.sh
@@ -1,8 +1,19 @@
 
 DOVECOT_MAIN_CONF="/etc/dovecot/dovecot.conf"
 TMP_DOVECOT_MAIN_CONF="/tmp/dovecot/dovecot.conf"
-EDIT_DOVECOT_MAIN_CONF="/data/config/dovecot/dovecot.conf"
-TMP_SLAPD_CONF="/tmp/slapd.conf"
-EDIT_ODBC_CONF="/data/config/odbc.ini"
-ODBC_CONF="/etc/odbc.ini"
-TMP_ODBC_CONF="/tmp/odbc.ini"
+DOVECOT_LDAP_CONF="/etc/dovecot/dovecot-ldap.conf"
+TMP_DOVECOT_LDAP_CONF="/tmp/dovecot/dovecot-ldap.conf"
+
+
+if [ ! -e $DOVECOT_MAIN_CONF ]; then
+	cp $TMP_DOVECOT_MAIN_CONF $DOVECOT_MAIN_CONF
+fi
+
+if [ ! -e $DOVECOT_LDAP_CONF ]; then
+	cp $TMP_DOVECOT_LDAP_CONF $DOVECOT_LDAP_CONF
+fi
+
+
+dovecot
+
+tail -f /var/log/dovecot/info.log
