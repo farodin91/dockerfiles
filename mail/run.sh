67753d0ab7f4d803273b18d3429ee544de466fe2
Jan Jansen <farodin91@googlemail.com>
2015-04-16 13:52:39 +0200

    clean update

--- mail/run.sh
+++ mail/run.sh
@@ -1,24 +1,31 @@
 
 DOVECOT_MAIN_CONF="/etc/dovecot/dovecot.conf"
-TMP_DOVECOT_MAIN_CONF="/tmp/dovecot/dovecot.conf"
+DOVECOT_TMP_MAIN_CONF="/tmp/dovecot/dovecot.conf"
 DOVECOT_LDAP_CONF="/etc/dovecot/dovecot-ldap.conf"
-TMP_DOVECOT_LDAP_CONF="/tmp/dovecot/dovecot-ldap.conf"
+DOVECOT_TMP_LDAP_CONF="/tmp/dovecot/dovecot-ldap.conf"
+POSTFIX_MAIN_CONF="/etc/postfix/main.cf"
+POSTFIX_MAIN_DIR="/etc/postfix/"
+POSTFIX_TMP_DIR="/tmp/postfix/"
 
 echo "CONFIGURE DOVECOT"
 if [ ! -e $DOVECOT_MAIN_CONF ]; then
-	cp $TMP_DOVECOT_MAIN_CONF $DOVECOT_MAIN_CONF
+	cp $DOVECOT_TMP_MAIN_CONF $DOVECOT_MAIN_CONF
 fi
 
 if [ ! -e $DOVECOT_LDAP_CONF ]; then
-	cp $TMP_DOVECOT_LDAP_CONF $DOVECOT_LDAP_CONF
+	cp $DOVECOT_TMP_LDAP_CONF $DOVECOT_LDAP_CONF
 fi
 
 echo "CONFIGURE POSTFIX"
+if [ ! -e $POSTFIX_MAIN_CONF ]; then
+	cp 
+fi
 
 echo "START POSTFIX"
 postfix start
 
 echo "START DOVECOT"
+rm /var/log/dovecot/info.log
 dovecot
 
 tail -f /var/log/dovecot/info.log
