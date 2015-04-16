0771137bbf948b528a25bcab257f8a50a54ded27
Jan Jansen <farodin91@googlemail.com>
2015-04-16 12:43:30 +0200

    add mail config

--- mail/run.sh
+++ mail/run.sh
@@ -4,7 +4,7 @@ TMP_DOVECOT_MAIN_CONF="/tmp/dovecot/dovecot.conf"
 DOVECOT_LDAP_CONF="/etc/dovecot/dovecot-ldap.conf"
 TMP_DOVECOT_LDAP_CONF="/tmp/dovecot/dovecot-ldap.conf"
 
-
+echo "CONFIGURE DOVECOT"
 if [ ! -e $DOVECOT_MAIN_CONF ]; then
 	cp $TMP_DOVECOT_MAIN_CONF $DOVECOT_MAIN_CONF
 fi
@@ -13,7 +13,12 @@ if [ ! -e $DOVECOT_LDAP_CONF ]; then
 	cp $TMP_DOVECOT_LDAP_CONF $DOVECOT_LDAP_CONF
 fi
 
+echo "CONFIGURE POSTFIX"
 
+echo "START POSTFIX"
+postfix start
+
+echo "START DOVECOT"
 dovecot
 
 tail -f /var/log/dovecot/info.log
