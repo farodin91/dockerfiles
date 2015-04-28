33b8b6d59ba64ec5f92a6511e6c1835d5754ece2
Jan Jansen <farodin91@googlemail.com>
2015-04-28 12:56:14 +0200

    update run

--- cloud/run.sh
+++ cloud/run.sh
@@ -2,11 +2,14 @@
 
 
 echo "CONFIGURE OPEN-XCHANGE"
-if [ls -l /opt/open-xchange/etc/ | grep -c ^- > 0] ; then
-	cp -r /tmp/etc/ /opt/open-xchnage/etc 
+if[ ls -l /opt/open-xchange/etc/ | grep -c ^- > 0] ; then
+	cp -r /tmp/etc/ /opt/open-xchange/etc 
 fi
 
+echo "CONFIGURE SUPERVISOR"
+chmod +x /data/open-xchange.sh
 
 
-ehco "START OPEN-XCHANGE"
+
+echo "START OPEN-XCHANGE"
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
