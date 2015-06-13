386842ed3faf290922ba9ff3513de560e8e95d12
train-Hammer <train-Hammer@web.de>
2015-06-14 01:13:19 +0200

    host ip

--- ttt/run.sh
+++ ttt/run.sh
@@ -3,4 +3,4 @@ if [ ! -e /opt/ttt/srcds_run ]; then
 	bash /opt/steamcmd/steamcmd.sh \+login anonymous \+force_install_dir /opt/ttt \+app_update 4020 \+quit
 fi
 cp /opt/ttt/bin/libsteam.so /root/.steam/sdk32
-/opt/ttt/srcds_run -console -maxplayers 16 -game garrysmod +gamemode terrortown +map gm_construct -authkey ${AUTH_KEY} +host_workshop_collection ${HOST_COLLECTION}
+/opt/ttt/srcds_run -console -maxplayers 16 +ip 192.168.x.x+ip 192.168.x.x -game garrysmod +gamemode terrortown +ip ${HOST_IP} +map gm_construct -authkey ${AUTH_KEY} +host_workshop_collection ${HOST_COLLECTION}
