58218652db720a6e9ec71f46d4a2c5d53886c4bf
train-Hammer <train-Hammer@web.de>
2015-06-13 23:26:12 +0200

    tests

---
+++ ttt/run.sh
@@ -0,0 +1,5 @@
+
+if [ ! -e /opt/ttt/srcds_run ]; then
+	bash /opt/steamcmd/steamcmd.sh \+login anonymous \+force_install_dir /opt/ttt \+app_update 4020 \+quit
+fi
+/opt/ttt/srcds_run -console -maxplayers 16 -game garrysmod +gamemode terrortown +map gm_construct
